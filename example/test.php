6d6d69e03c311f318135250f919429f6679acc99
Fernando Tauscheck <ftauscheck@users.noreply.github.com>
2021-08-06 21:05:53 -0300

    Update test.php

--- example/test.php
+++ example/test.php
@@ -1,17 +1,27 @@
 <?php
+  include('color.php');
+  echo "\e[H\e[J";
+  $colors = new Colors();
+	echo "\n".$colors->head("╔══════════════════════════════════════════════════════════════════════════════╗");
+	echo "\n".$colors->head("║                                                                              ║");
+	echo "\n".$colors->head("║     php-cli-color_v2 Library                                                 ║");
+  echo "\n".$colors->head("║                                                                              ║");
+	echo "\n".$colors->head("║     ver: 1.0 - Initial release (after fork of wujunze/php-cli-color)         ║");
+	echo "\n".$colors->head("║                                                                              ║");
+  echo "\n".$colors->head("╟──────────────────────────────────────────────────────────────────────────────╢");
+  echo "\n".$colors->head("║                                                                              ║");
+	echo "\n".$colors->head("║        https://github.com/ftauscheck/php-cli-color_v2                        ║");
+	echo "\n".$colors->head("║                                                                              ║");
+	echo "\n".$colors->head("╚══════════════════════════════════════════════════════════════════════════════╝")."\n\n";
 
-require_once __DIR__.'./../vendor/autoload.php';
-
-$colors = new Wujunze\Colors();
-// Test some basic printing with Colors class
-echo $colors->getColoredString('Testing Colors class, this is purple string on yellow background.', 'purple',
-        'yellow').PHP_EOL;
-echo $colors->getColoredString('Testing Colors class, this is blue string on light gray background.', 'blue',
-        'light_gray').PHP_EOL;
-echo $colors->getColoredString('Testing Colors class, this is red string on black background.', 'red',
-        'black').PHP_EOL;
-echo $colors->getColoredString('Testing Colors class, this is cyan string on green background.', 'cyan',
-        'green').PHP_EOL;
-echo $colors->getColoredString('Testing Colors class, this is cyan string on default background.', 'cyan').PHP_EOL;
-echo $colors->getColoredString('Testing Colors class, this is default string on cyan background.', null,
-        'cyan').PHP_EOL;
+  echo "\n".$colors->notice("Notice message.");
+  echo "\n".$colors->warn("Warning message.");
+  echo "\n".$colors->error("Error message.");
+  echo "\n".$colors->fatalerror("Fatal Error message.");
+  echo "\n".$colors->success("Success message");
+  
+  echo "\n".$colors->getColoredString("Colored string: purple string on yellow background.", "purple", "yellow");
+  echo "\n".$colors->getColoredString("Colored string: blue string on light gray background.", "blue", "light_gray");
+  echo "\n".$colors->getColoredString("Colored string: red string on black background (error function).", "red", "black");
+  echo "\n";
+?>
